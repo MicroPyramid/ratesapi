0e506e89e4bf2cf0a7fe6560171466e2c4c5c45c
yosef langer <yosef@doar.systems>
2019-11-05 13:39:43 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,16 +1,16 @@
-var MongoClient = require('mongodb').MongoClient;
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
 
-var db_url = "mongodb://localhost:27017/currency";
+let db_url = "mongodb://localhost:27017/currency";
 
 http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
+    let q = url.parse(req.url, true);
     pathname = url.parse(req.url).pathname;
     if (pathname.match(/^\/style.css\/$/) || pathname.match(/^\/style.css$/)) {
         res.writeHead(200, {'Content-type' : 'text/css'});
-        var fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
+        let fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
         res.write(fileContents);
         res.end();
     }
@@ -28,27 +28,38 @@ http.createServer(function (req, res) {
     }
     else if (pathname.match(/^\/api\/latest\/$/) || pathname.match(/^\/api\/latest$/)){
         MongoClient.connect(db_url, function(err, dbo) {
-        var db = dbo.db("currency");
-        var collection = db.collection('prices');
-        fields = {base:1, date:1, _id:0, rates: 1}
-        query = {'base': "EUR"}
+        const db = dbo.db("currency");
+        const collection = db.collection('prices');
+        let fields = {base:1, date:1, _id:0, rates: 1}
+        let query = {'base': "EUR"}
 
         if(q.query['base']){
-            query = {'base': q.query['base'].toUpperCase()}
+            query = {'base': q.query['base'].toUpperCase()};
         }
+		if(q.query['from']&&q.query['to']&&q.query['amount']){
+            conversion = {'from': q.query['from'].toUpperCase(),'to': q.query['to'].toUpperCase(),'amount': parseFloat(q.query['amount'].toUpperCase())};
+			query['base'] =  q.query['from'].toUpperCase();
+		}
+		
+		
         if(q.query['symbols']){
             fields = {base:1, date:1, _id:0}
-            symbols = q.query['symbols'].split(',')
+            let symbols = q.query['symbols'].split(',')
             for (each in symbols){
                 symbol = 'rates.' + symbols[each].toUpperCase()
                 fields[symbol] = 1;
             }
         }
-        var cursor = collection.find(query, {fields: fields}).sort({"date": -1}).limit(1).toArray(function(err, result) {
+        let cursor = collection.find(query, {fields: fields}).sort({"date": -1}).limit(1).toArray(function(err, result) {
             if (err) throw err;
             dbo.close();
             if(result[0]){
                 res.writeHead(200, {'Content-Type': 'text/json'});
+				
+				if(result[0].rates&&conversion){
+					result[0].conversion={...conversion,result:result[0].rates[conversion.to]};
+				}
+				
                 res.write(JSON.stringify(result[0]));
             }else{
                 res.writeHead(400, {'Content-Type': 'text/json'});
@@ -60,22 +71,22 @@ http.createServer(function (req, res) {
 
     }else if (pathname.match(/^\/api\/\d{4}([./-])\d{2}\1\d{2}$/)){
         MongoClient.connect(db_url, function(err, dbo) {
-          var db = dbo.db("currency");
-          var collection = db.collection('prices');
-            fields = {base:1, date:1, _id:0, rates: 1}
-            query = {'base': "EUR", 'date': { $lte: req.url.split('/api/')[1].split('?')[0].toString() } }
+          const db = dbo.db("currency");
+          const collection = db.collection('prices');
+          let fields = {base:1, date:1, _id:0, rates: 1}
+          let query = {'base': "EUR", 'date': { $lte: req.url.split('/api/')[1].split('?')[0].toString() } }
             if(q.query['base']){
                 query['base']= q.query['base'].toUpperCase()
             }
             if(q.query['symbols']){
                 fields = {base:1, date:1, _id:0}
-                symbols = q.query['symbols'].split(',')
+                let symbols = q.query['symbols'].split(',')
                 for (each in symbols){
                     symbol = 'rates.' + symbols[each].toUpperCase()
                     fields[symbol] = 1;
                 }
             }
-          var cursor = collection.find(query, {fields: fields}).sort({date: -1}).toArray(function(err, result) {
+          let cursor = collection.find(query, {fields: fields}).sort({date: -1}).toArray(function(err, result) {
             if (err){
                 throw err;
             }
@@ -85,7 +96,7 @@ http.createServer(function (req, res) {
                 res.write(JSON.stringify(result[0]));
             }else{
                 res.writeHead(400, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify({"error":"Invalid base or symbols"}))
+                res.write(JSON.stringify({"error":"Invalid base or symbols"}));
             }
             res.end();
           });
@@ -95,4 +106,4 @@ http.createServer(function (req, res) {
         res.write('Contents you are looking are Not Found');
         res.end();
   }
-}).listen(8080);
+}).listen(process.argv.slice(2).length>0?process.argv.slice(2)[0]:8080);
