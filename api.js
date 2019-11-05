402045d91ed351bacd96a77f6534e182dfde6d4f
yosef langer <yosef@doar.systems>
2019-11-05 14:26:22 +0200

    modified code into a bit more modern js es6 , replacing var with let and const,  also some variables were global unnecresarily. made port 8080 optional, just specify on the command line number eg node api.js 8080 .   added additional api option to convert currency directly  like so:   api/latest?from=usd&to=eur&amount=5  responds the conversion immediatly

--- api.js
+++ api.js
@@ -1,109 +1,109 @@
-let MongoClient = require('mongodb').MongoClient;
-let http = require('http');
-let url = require('url');
-let fs = require('fs');
-
-let db_url = "mongodb://localhost:27017/currency";
-
+const MongoClient = require('mongodb').MongoClient;
+const http = require('http');
+const url = require('url');
+const db_url = "mongodb://localhost:27017/currency";
+const fs = require("fs");
 http.createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    pathname = url.parse(req.url).pathname;
+    const q = url.parse(req.url, true);
+    const pathname = url.parse(req.url).pathname;
     if (pathname.match(/^\/style.css\/$/) || pathname.match(/^\/style.css$/)) {
-        res.writeHead(200, {'Content-type' : 'text/css'});
-        let fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
+        res.writeHead(200, {'Content-type': 'text/css'});
+        const fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
         res.write(fileContents);
         res.end();
-    }
-    else if (req.url === '/'){
-        fs.readFile('./home.html', null, function(err, data){
-            if(err){
+    } else if (req.url === '/') {
+        fs.readFile('./home.html', null, function (err, data) {
+            if (err) {
                 res.writeHead(404);
                 res.write('Contents you are looking are Not Found');
                 res.end();
-            }else{
+            } else {
                 res.write(data);
                 res.end();
             }
         });
-    }
-    else if (pathname.match(/^\/api\/latest\/$/) || pathname.match(/^\/api\/latest$/)){
-        MongoClient.connect(db_url, function(err, dbo) {
-        const db = dbo.db("currency");
-        const collection = db.collection('prices');
-        let fields = {base:1, date:1, _id:0, rates: 1}
-        let query = {'base': "EUR"}
+    } else if (pathname.match(/^\/api\/latest\/$/) || pathname.match(/^\/api\/latest$/)) {
+        MongoClient.connect(db_url, function (err, dbo) {
+            const db = dbo.db("currency");
+            const collection = db.collection('prices');
+            let fields = {base: 1, date: 1, _id: 0, rates: 1}
+            let query = {'base': "EUR"}
+            let conversion = null;
+            if (q.query['base']) {
+                query = {'base': q.query['base'].toUpperCase()};
+            }
+            if (q.query['from'] && q.query['to'] && q.query['amount']) {
+                conversion = {
+                    'from': q.query['from'].toUpperCase(),
+                    'to': q.query['to'].toUpperCase(),
+                    'amount': parseFloat(q.query['amount'].toUpperCase())
+                };
+                query['base'] = q.query['from'].toUpperCase();
+            }
 
-        if(q.query['base']){
-            query = {'base': q.query['base'].toUpperCase()};
-        }
-		if(q.query['from']&&q.query['to']&&q.query['amount']){
-            conversion = {'from': q.query['from'].toUpperCase(),'to': q.query['to'].toUpperCase(),'amount': parseFloat(q.query['amount'].toUpperCase())};
-			query['base'] =  q.query['from'].toUpperCase();
-		}
-		
-		
-        if(q.query['symbols']){
-            fields = {base:1, date:1, _id:0}
-            let symbols = q.query['symbols'].split(',')
-            for (each in symbols){
-                symbol = 'rates.' + symbols[each].toUpperCase()
-                fields[symbol] = 1;
+
+            if (q.query['symbols']) {
+                fields = {base: 1, date: 1, _id: 0}
+                let symbols = q.query['symbols'].split(',')
+                for (const each in symbols) {
+                    const symbol = 'rates.' + symbols[each].toUpperCase()
+                    fields[symbol] = 1;
+                }
             }
-        }
-        let cursor = collection.find(query, {fields: fields}).sort({"date": -1}).limit(1).toArray(function(err, result) {
-            if (err) throw err;
-            dbo.close();
-            if(result[0]){
-                res.writeHead(200, {'Content-Type': 'text/json'});
-				
-				if(result[0].rates&&conversion){
-					result[0].conversion={...conversion,result:result[0].rates[conversion.to]};
-				}
-				
-                res.write(JSON.stringify(result[0]));
-            }else{
-                res.writeHead(400, {'Content-Type': 'text/json'});
-                res.write(JSON.stringify({"error":"Invalid base or symbols"}))
-            }
-            res.end();
+            collection.find(query, {fields: fields}).sort({"date": -1}).limit(1).toArray(function (err, result) {
+                if (err) throw err;
+                dbo.close();
+                if (result[0]) {
+                    res.writeHead(200, {'Content-Type': 'text/json'});
+
+                    if (result[0].rates && conversion) {
+                        conversion.result = result[0].rates[conversion.to] * conversion.amount;
+                        res.write(JSON.stringify(conversion));
+                    } else
+                        res.write(JSON.stringify(result[0]));
+                } else {
+                    res.writeHead(400, {'Content-Type': 'text/json'});
+                    res.write(JSON.stringify({"error": "Invalid base or symbols"}))
+                }
+                res.end();
             });
         });
 
-    }else if (pathname.match(/^\/api\/\d{4}([./-])\d{2}\1\d{2}$/)){
-        MongoClient.connect(db_url, function(err, dbo) {
-          const db = dbo.db("currency");
-          const collection = db.collection('prices');
-          let fields = {base:1, date:1, _id:0, rates: 1}
-          let query = {'base': "EUR", 'date': { $lte: req.url.split('/api/')[1].split('?')[0].toString() } }
-            if(q.query['base']){
-                query['base']= q.query['base'].toUpperCase()
+    } else if (pathname.match(/^\/api\/\d{4}([./-])\d{2}\1\d{2}$/)) {
+        MongoClient.connect(db_url, function (err, dbo) {
+            const db = dbo.db("currency");
+            const collection = db.collection('prices');
+            let fields = {base: 1, date: 1, _id: 0, rates: 1}
+            let query = {'base': "EUR", 'date': {$lte: req.url.split('/api/')[1].split('?')[0].toString()}}
+            if (q.query['base']) {
+                query['base'] = q.query['base'].toUpperCase()
             }
-            if(q.query['symbols']){
-                fields = {base:1, date:1, _id:0}
+            if (q.query['symbols']) {
+                fields = {base: 1, date: 1, _id: 0}
                 let symbols = q.query['symbols'].split(',')
-                for (each in symbols){
+                for (each in symbols) {
                     symbol = 'rates.' + symbols[each].toUpperCase()
                     fields[symbol] = 1;
                 }
             }
-          let cursor = collection.find(query, {fields: fields}).sort({date: -1}).toArray(function(err, result) {
-            if (err){
-                throw err;
-            }
-            dbo.close();
-            if(result[0]){
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify(result[0]));
-            }else{
-                res.writeHead(400, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify({"error":"Invalid base or symbols"}));
-            }
-            res.end();
-          });
+            collection.find(query, {fields: fields}).sort({date: -1}).toArray(function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                dbo.close();
+                if (result[0]) {
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify(result[0]));
+                } else {
+                    res.writeHead(400, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify({"error": "Invalid base or symbols"}));
+                }
+                res.end();
+            });
         });
-    }else{
+    } else {
         res.writeHead(404);
         res.write('Contents you are looking are Not Found');
         res.end();
-  }
-}).listen(process.argv.slice(2).length>0?process.argv.slice(2)[0]:8080);
+    }
+}).listen(process.argv.slice(2).length > 0 ? process.argv.slice(2)[0] : 8080);
